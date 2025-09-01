a94e9dfe0a1964f54cf24ee0fe16010b24e1ad66
GolemOnce <shh3350@gmail.com>
2025-09-01 13:34:31 +0900

    타이니 서버 구현 원본

--- GolemOnce/tiny/tiny.c
+++ GolemOnce/tiny/tiny.c
@@ -143,3 +143,51 @@ int parse_uri(char *uri, char *filename, char *cgiargs){
     return 0;
   }
 }
+
+void serve_static(int fd, char *filename, int filesize){
+  int srcfd;
+  char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+
+  get_filetype(filename, filetype);
+  sprintf(buf, "HTTP/1.0 200 OK \r\n");
+  sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+  sprintf(buf, "%sConnection: close\r\n", buf);
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+
+  Rio_writen(fd, buf, strlen(buf));
+  printf("Response headers:\n");
+  printf("%s", buf);
+
+
+  srcfd = Open(filename, O_RDONLY, 0);
+  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  Close(srcfd);
+  Rio_writen(fd, srcp, filesize);
+  Nummap(*srcp, filesize);
+}
+
+void get_filetype(char *filename, char *filetype){
+  if (strstr(filename, ".html")) strcpy(filetype, "text/html");
+  else if (strstr(filename, ".gif")) strcpy (filetype, "image/gif");
+  else if (strstr(filename, ".png")) strcpy (filetype, "image/png");
+  else if (strstr(filename, ".jpg")) strcpy (filetype, "image/jpeg");
+  else strcpy(filetype, "text/plain");
+}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs){
+  char buf[MAXLINE], *emptylist[] = { NULL };
+
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Server: Tiny Web Server\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+
+  if (Fork() == 0){
+    setenv("QUERY_STRING", cgiargs, 1);
+    Dup2(fd, STDOUT_FILENO);
+    Execve(filename, emptylist, environ);
+  }
+  Wait(NULL);
+}
