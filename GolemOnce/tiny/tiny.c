3d6ee8bee4f00ed03e1e9682914c8aafeb440688
GolemOnce <shh3350@gmail.com>
2025-09-01 20:15:30 +0900

    숙제문제 11.6,7,9,10,11

--- GolemOnce/tiny/tiny.c
+++ GolemOnce/tiny/tiny.c
@@ -8,12 +8,12 @@
  */
 #include "csapp.h"
 
-void doit(int fd) {};
+void doit(int fd);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
+void serve_static(int fd, char *filename, int filesize, char *method); // 숙제문제 11.11 매개변수 method추가
 void get_filetype(char *filename, char *filetype);
-void serve_dynamic(int fd, char *filename, char *cgiargs);
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method); // 숙제문제 11.11 매개변수 method추가
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv) {
@@ -38,9 +38,22 @@ int main(int argc, char **argv) {
                 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
     doit(connfd);  // line:netp:tiny:doit
+    // echo(connfd);
     Close(connfd); // line:netp:tiny:close
   }
 }
+
+void echo(int connfd){
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        if (strcmp(buf, "\r\n") == 0) break;
+        Rio_writen(connfd, buf, n);
+    }
+} // 숙제문제 11.6
 
 void doit(int fd) {
   int is_static;
@@ -56,7 +69,7 @@ void doit(int fd) {
   sscanf(buf, "%s %s %s", method, uri, version);
   
   // GET 요청이 아닌 다른 요청 입력시 에러 반환 (tiny서버는 GET메소드만 지원함)
-  if (strcasecmp(method, "GET")) {
+  if (strcasecmp(method, "GET") || strcasecmp(method, "HEAD")) { // 숙제문제 11.11 method HEAD 추가
     clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
     return;
   }
@@ -75,14 +88,14 @@ void doit(int fd) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read this file");
       return;
     }
-    serve_static(fd, filename, sbuf.st_size);
+    serve_static(fd, filename, sbuf.st_size, method); // 숙제문제 11.11 매개변수 method추가
   }
   else { // 동적 컨텐츠 제공
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { // regular file인지, 실행 권한이 있는지 여부 체크
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
       return;
     }
-    serve_dynamic(fd, filename, cgiargs);
+    serve_dynamic(fd, filename, cgiargs, method); // 숙제문제 11.11 매개변수 method추가
   }
 }
 
@@ -129,7 +142,8 @@ int parse_uri(char *uri, char *filename, char *cgiargs) {
     return 1;
   }
   else { // 동적 컨텐츠
-    ptr = index(uri, '?');
+    // ptr = index(uri, '?');
+    ptr = strchr(uri, '?');
     if (ptr) {
       strcpy(cgiargs, ptr+1);
       *ptr = '\0';
@@ -141,7 +155,7 @@ int parse_uri(char *uri, char *filename, char *cgiargs) {
   }
 }
 
-void serve_static(int fd, char *filename, int filesize) {
+void serve_static(int fd, char *filename, int filesize, char *method) { // 숙제문제 11.11 매개변수 method추가
   int srcfd;
   char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
@@ -157,12 +171,16 @@ void serve_static(int fd, char *filename, int filesize) {
   printf("Response headers:\n");
   printf("%s", buf);
 
-
-  srcfd = Open(filename, O_RDONLY, 0);
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-  Close(srcfd);
-  Rio_writen(fd, srcp, filesize);
-  Nummap(*srcp, filesize);
+  if (strcasecmp(method, "GET") == 0) { // 숙제문제 11.11 method가 GET일 경우에만 body출력
+    srcfd = Open(filename, O_RDONLY, 0);
+    // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    srcp = (char *)malloc(filesize); // 숙제문제 11.9 mmap > malloc + rio_readn
+    Rio_readn(srcfd, srcp, filesize);
+    Close(srcfd);
+    Rio_writen(fd, srcp, filesize);
+    // Nummap(srcp, filesize);
+    free(srcp);
+  }
 }
 
 void get_filetype(char *filename, char *filetype) {
@@ -170,10 +188,11 @@ void get_filetype(char *filename, char *filetype) {
   else if (strstr(filename, ".gif")) strcpy (filetype, "image/gif");
   else if (strstr(filename, ".png")) strcpy (filetype, "image/png");
   else if (strstr(filename, ".jpg")) strcpy (filetype, "image/jpeg");
+  else if (strstr(filename, ".mpg")) strcpy (filetype, "video/mpg"); // 숙제문제 11.7 MPG 비디오 파일 처리
   else strcpy(filetype, "text/plain");
 }
 
-void serve_dynamic(int fd, char *filename, char *cgiargs) {
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method) {
   char buf[MAXLINE], *emptylist[] = { NULL };
 
   sprintf(buf, "HTTP/1.0 200 OK\r\n");
@@ -183,6 +202,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
 
   if (Fork() == 0) {
     setenv("QUERY_STRING", cgiargs, 1);
+    setenv("REQUEST_METHOD", method, 1); // 숙제문제 11.11 HEAD
     Dup2(fd, STDOUT_FILENO);
     Execve(filename, emptylist, environ);
   }
